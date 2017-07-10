c838c7a37e274c9ea21661d823c4d02823fd2980
Julian Vilas <julian.vilas@gmail.com>
2017-07-10 21:09:19 +0200

    Update ctf vagrant box to zesty

--- ctf/install.sh
+++ ctf/install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-# dependencies
+echo 'Installing dependencies'
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends curl git build-essential gdb gnupg python python-pip libpython-dev python-qt4 cmake virtualenvwrapper libffi-dev
+sudo apt-get install -y --no-install-recommends curl git build-essential gdb gnupg python python-pip libpython-dev python-qt4 cmake virtualenvwrapper libffi-dev golang-1.8-go
+
+export PATH=/usr/lib/go-1.8/bin:$PATH
 
 # vim
+echo 'Tunning VIM'
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 curl -LSso ~/.vimrc https://raw.githubusercontent.com/julianvilas/dotfiles/master/vimrc
@@ -14,25 +17,18 @@ git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
-# go
-git clone https://go.googlesource.com/go ~/go1.4
-cd ~/go1.4 && git checkout release-branch.go1.4
-cd ~/go1.4/src && ./make.bash
-
-cp -R ~/go1.4 ~/go
-cd ~/go && git clean -dfx && git reset --hard && git checkout go1.8.3
-cd ~/go/src && ./make.bash
-
 mkdir -p ~/gocode
-GOPATH=$HOME/gocode ~/go/bin/go get golang.org/x/tools/... github.com/nsf/gocode
+echo 'Getting golang tools'
+GOPATH=$HOME/gocode go get -v golang.org/x/tools/... github.com/nsf/gocode
 
 ~/gocode/src/github.com/nsf/gocode/vim/pathogen_update.sh
 
 echo 'export GOPATH=$HOME/gocode
-export PATH=$HOME/go/bin:$GOPATH/bin:$PATH
+export PATH=/usr/lib/go-1.8/bin:$GOPATH/bin:$PATH
 export EDITOR=vim' >> ~/.profile
 
 # rvm, ruby
+echo 'Installing ruby and rvm'
 curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 curl -sSL https://get.rvm.io | bash -s stable
 . ~/.rvm/scripts/rvm
@@ -50,6 +46,7 @@ git clone https://github.com/vivisect/vivisect.git ~/src/vivisect
 git clone https://github.com/jjyg/metasm.git ~/src/metasm
 
 # radare2
+echo 'Compiling r2'
 git clone https://github.com/radare/radare2.git ~/src/radare2
 cd ~/src/radare2 && ./sys/install.sh
 
@@ -58,14 +55,17 @@ git clone https://github.com/longld/peda.git ~/src/peda
 echo "set disassembly-flavor intel" > ~/.gdbinit
 echo "source ~/src/peda/peda.py" >> ~/.gdbinit
 
+pip install -U pip setuptools
+pip install --upgrade pip
+
 # capstone, keystone
-sudo pip install capstone keystone-engine
+pip install --user capstone keystone-engine
 
 # ropgadget
-sudo pip install ropgadget
+pip install --user ropgadget
 
 # pwntools
-sudo pip install pwntools
+pip install --user pwntools
 
 # angr
-sudo pip install angr
+pip install --user angr
